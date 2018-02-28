8167081b40cf7b560528b3bc8879b6bd824b7857
MaJoy <MaJoy@192.168.25.13>
2018-02-28 02:51:09 -0300

    incluidas todas funções para manipulação dos dados da arvore contidas no capitulo 12 do licro do cormem, além de funções auxiliares

--- binaryTreeSchedule/main.c
+++ binaryTreeSchedule/main.c
@@ -19,10 +19,9 @@ Profº: Raphael Peccin Torchelsen
 //Definindo o tipo abstrato 'tree'
 struct tree{
     char data[sizeOfName];//string armazenada
-    struct tree *left;    //Folha/Nó da esquerda
-    struct tree *right;   //Folha/Nó da direita
-    struct tree *root; //raiz do nó
-    int  height;
+    struct tree *left;   //filho da esquerda
+    struct tree *right;  //filho da direita
+    struct tree *parent; //pai
 };
 
 /*
@@ -31,6 +30,7 @@ Parametros:
     root    :Folha/Nó Raiz
     thisTree:Folha/Nó que será inserido na estrutura
     data    :Dado pertencente à Folha/Nó que será incluída(o)
+ * Algoritmo baseado no livro C completo e total
 */
 struct tree *makeTree(struct tree *root, struct tree *thisTree, char *data){
     //Nova raiz
@@ -44,20 +44,17 @@ struct tree *makeTree(struct tree *root, struct tree *thisTree, char *data){
         //Caso a subarvore ainda não exista então é criada uma arvore nova a partir deste(a) Folha/Nó
         thisTree->left = NULL;
         thisTree->right = NULL;
-        thisTree->root = NULL;
-        thisTree->height =0;
+        thisTree->parent = NULL;
         strcpy(thisTree->data, data);
         //Retorna a subarvore atual como Folha/Nó Raiz, caso ela mesma não possua Raiz
         if(!root) return thisTree;
         //Posiciona a subarvore atual a esquerda da raiz caso a string seja menor que a string da raiz
         if(strcmp(data, root->data)<0){
             root->left = thisTree;
-            thisTree->root = root;
         }
         //Caso contrário posiciona a subarvore atual a direita da raiz
         else{
             root->right = thisTree;
-            thisTree->root = root;
         }
         return thisTree;
     }
@@ -66,46 +63,106 @@ struct tree *makeTree(struct tree *root, struct tree *thisTree, char *data){
     else makeTree(thisTree, thisTree->right, data);
 }
 
-void inOrderTree(struct tree *root){
+/* Percurso de arvore in-ordem
+ * Algoritmo que imprime a chave da raiz de uma subarvore entre a impressão dos 
+ * valores em em sua subarvore esquerda e a impressão dos valores em sua subarvore
+ * direita
+ * Algoritmo baseado no livro do Thomas Cormem*/
+void inOrderTreeWalk(struct tree *root){
     if(!root) return;
-    inOrderTree(root->left);
+    inOrderTreeWalk(root->left);
     if(root->data) printf("%s", root->data);
-    inOrderTree(root->right);
+    inOrderTreeWalk(root->right);
 }
-
-void preOrderTree(struct tree *root){
-    if(!root) return;
-    if(root->data) printf("%s", root->data);
-    preOrderTree(root->left);
-    preOrderTree(root->right);
-}
-
-void postOrderTree(struct tree *root){
-    if(!root) return;
-    postOrderTree(root->left);
-    postOrderTree(root->right);
-    if(root->data) printf("%s", root->data);
-}
-
-void printTree(struct tree *thisTree, int l){
-    if(thisTree==NULL) return;
-
-    printTree(thisTree->left, l+1);
+void inOrderPrintTree(struct tree *thisTree, int l){ 
+    inOrderPrintTree(thisTree->left, l+1);
     for(int i=0; i<l; ++i) printf("  ");
     printf("%s\n", thisTree->data);
-    printTree(thisTree->right, l+1);
+    inOrderPrintTree(thisTree->right, l+1);    
 }
-/*Função que percorre a arvore procurando um nó que contém a string pesquisada
- retorna NULL caso não encontre*/
-struct tree *searchInTree(struct tree *thisTree, char keySearch[sizeOfName]){
+/* Percurso de arvore em pré-ordem
+ * Algoritmo que imprime a chave da raiz de uma subarvore antes dos valores das suas
+ * subarvores
+ * Algoritmo baseado no livro do Thomas Cormem*/
+void preOrderTreeWalk(struct tree *root){
+    if(!root) return;
+    if(root->data) printf("%s", root->data);
+    preOrderTreeWalk(root->left);
+    preOrderTreeWalk(root->right);
+}
+void preOrderPrintTree(struct tree *thisTree, int l){ 
+    for(int i=0; i<l; ++i) printf("  ");
+    printf("%s\n", thisTree->data);
+    preOrderPrintTree(thisTree->left, l+1);
+    preOrderPrintTree(thisTree->right, l+1);       
+}
+/* Percurso de arvore em pós-ordem
+ * Algoritmo que imprime a chave da raiz de uma subarvore depois dos valores das suas
+ * subarvores
+ * Algoritmo baseado no livro do Thomas Cormem*/
+void postOrderTreeWalk(struct tree *root){
+    if(!root) return;
+    postOrderTreeWalk(root->left);
+    postOrderTreeWalk(root->right);
+    if(root->data) printf("%s", root->data);
+}
+void postOrderPrintTree(struct tree *thisTree, int l){ 
+    postOrderPrintTree(thisTree->left, l+1);
+    postOrderPrintTree(thisTree->right, l+1);        
+    for(int i=0; i<l; ++i) printf("  ");
+    printf("%s\n", thisTree->data);
+}
+/*Imprimir arvore
+ * Imprimi uma arvore de forma hierarquica de acordo com a ordem de percurso
+ * definida, as opções são:
+ * 0- in-ordem
+ * 1- pré-ordem
+ * 2- pós-ordem */
+void printTree(struct tree *thisTree, int l, int orderWalk){
+    if(thisTree==NULL) return;    
+    if(orderWalk == 0)
+        inOrderPrintTree(thisTree, l);       
+    else if (orderWalk == 1)
+        preOrderPrintTree(thisTree, l);         
+    else if (orderWalk == 2)
+        postOrderPrintTree(thisTree, l);       
+    else return;    
+}
+/*Função que percorre a arvore procurando de forma recursiva um nó que contém a 
+ * string pesquisada e retorna NULL caso não encontre*/
+struct tree *recursiveSearchInTree(struct tree *thisTree, char keySearch[sizeOfName]){
     if (thisTree == NULL || strcmp(keySearch ,thisTree->data) == 0){
         return thisTree;
     }
     if(strcmp(keySearch ,thisTree->data) < 0){
-        return searchInTree(thisTree->left, keySearch);
+        return recursiveSearchInTree(thisTree->left, keySearch);
     }else{
-        return searchInTree(thisTree->right, keySearch);
+        return recursiveSearchInTree(thisTree->right, keySearch);
     }
+}
+/*Função que percorre a arvore procurando de forma iterativa um nó que contém a 
+ * string pesquisada e retorna NULL caso não encontre, esta versão é mais eficiente*/
+struct tree *iterativeSearchInTree(struct tree *thisTree, char keySearch[sizeOfName]){
+    while (thisTree != NULL || strcmp(keySearch ,thisTree->data) != 0){
+         if(strcmp(keySearch ,thisTree->data) < 0)
+             thisTree = thisTree->left;
+         else
+             thisTree = thisTree->right;
+    }
+    return thisTree;
+}
+/* Função que permite selecionar qual o algoritmo utilizado para  realizar as buscas
+ * de uma arvore binária as opções são:
+ * 0- algoritmo recursivo
+ * 1- algoritmo iterativo
+ **/
+struct tree *SearchInTree(struct tree *thisTree, char keySearch[sizeOfName], int searchMode){
+    if(searchMode == 0)
+        return recursiveSearchInTree(thisTree, keySearch);
+    else if(searchMode == 1)
+        return iterativeSearchInTree(thisTree, keySearch);
+    else
+        return NULL;
 }
 /*Função que retorna o menor nó da arvore*/
 struct tree *minimumNode(struct tree *thisTree){
@@ -123,31 +180,94 @@ struct tree *maximumNode(struct tree *thisTree){
 }
 /*Função que retorna o sucessor de um nó da arvore*/
 struct tree *sucessorNode(struct tree *thisTree){
-    struct tree *root;
+    struct tree *sucessor;
     
-    if(thisTree->right != NULL){
+    if(thisTree->right != NULL)
         return minimumNode(thisTree->right);
+    sucessor=thisTree->parent;
+    while(sucessor != NULL && thisTree == sucessor->left){
+        thisTree = sucessor;
+        sucessor = sucessor->parent;
     }
-    root=thisTree->root;
-    while(root != NULL && thisTree == root->left){
-        thisTree = root;
-        root = root->root;
-    }
-    return thisTree;
+    return sucessor;
 }
 /*Função que retorna o antecessor de um nó da arvore*/
 struct tree *predecessorNode(struct tree *thisTree){
-    struct tree *root;
-    
-    if(thisTree->left != NULL){
+    struct tree *predecessor;    
+    if(thisTree->left != NULL)
         return thisTree->left;
+    predecessor=thisTree->parent;
+    while(predecessor != NULL && thisTree == predecessor->right){
+        thisTree = predecessor;
+        predecessor = predecessor->parent;
     }
-    root=thisTree->root;
-    while(root != NULL && thisTree == root->right){
-        thisTree = root;
-        root = root->root;
+    return predecessor;
+}
+/*Função que adiciona um novo nó a arvore, assumindo que o argumento thisTree é
+ a raiz da árvore*/
+void insertNewNode(struct tree *thisTree, struct tree *newNode){
+    struct tree *temporaryNode=NULL;
+    if(!thisTree)
+        thisTree = newNode;
+    else{
+        while (thisTree != NULL){
+            temporaryNode = thisTree;
+            if(strcmp(newNode->data,thisTree->data)<0)
+                thisTree = thisTree->left;
+            else
+                thisTree = thisTree->right;        
+        }
+        newNode->parent = temporaryNode;
+        if(temporaryNode == NULL)
+            thisTree = newNode;
+        else if(strcmp(newNode->data, temporaryNode->data)<0)
+            temporaryNode->left = newNode;
+        else
+            temporaryNode->right = newNode;
     }
-    return thisTree;
+}
+    
+/*Função que aloca espaço na memória e cria um novo nó com a informação da chave*/
+struct tree *createNewNode(struct tree *newNode, char *data){
+    newNode = (struct tree *) malloc(sizeof(struct tree));
+    if(!newNode){
+        printf("Não foi possível alocar memória!");
+        exit(0);
+    }else
+        newNode->left = NULL;
+        newNode->right = NULL;
+        newNode->parent = NULL;
+        strcpy(newNode->data, data);        
+        return newNode;
+}
+/*Função que subistitui uma arvore por outra*/
+void transplantNode(struct tree *thisTree, struct tree *currentNode, struct tree *newNode){
+    if(currentNode->parent == NULL)
+        thisTree = newNode;
+    else if(currentNode == currentNode->parent->left)
+        currentNode->parent->left = newNode;
+    else
+        currentNode->parent->right = newNode;
+    if(newNode != NULL)
+        newNode->parent = currentNode->parent;
+}
+
+void deleteNode(struct tree *thisTree, struct tree *deletedNode){
+    struct tree *temporaryNode;
+    if(deletedNode->left == NULL)
+        transplantNode(thisTree, deletedNode, deletedNode->right);
+    else if(deletedNode->right == NULL)
+        transplantNode(thisTree, deletedNode, deletedNode->left);
+    else
+        temporaryNode = minimumNode(deletedNode->right);
+    if(temporaryNode->parent != deletedNode){
+        transplantNode(thisTree, temporaryNode, temporaryNode->right);
+        temporaryNode->right = deletedNode->right;
+        temporaryNode->right->parent = temporaryNode;
+    }
+    transplantNode(thisTree, deletedNode, temporaryNode);
+    temporaryNode->left = deletedNode->left;
+    temporaryNode->left->parent = temporaryNode;
 }
 
 /*Função que gira um nó para direita*/
@@ -161,7 +281,6 @@ void rightRotation(struct tree *thisTree){
     thisTree->left=temporaryTree;
     thisTree=qTree;
 }
-
 /*Função que gira um nó para esquerda*/
 void leftRotation(struct tree *thisTree){
     struct tree *temporaryTree;
@@ -173,36 +292,33 @@ void leftRotation(struct tree *thisTree){
     thisTree->right=temporaryTree;
     thisTree=qTree;
 }
-
 /*Função que gira um nó da direita para esquerda*/
 void rightToLeftRotation(struct tree *thisTree){
    
     rightRotation(thisTree->right);
     leftRotation(thisTree);
 }
-
 /*Função que gira um nó da esquerda para direita*/
 void leftToRightRotation(struct tree *thisTree){
     leftRotation(thisTree->right);
     rightRotation(thisTree);
 }
-
 /*Função que equilibra uma subarvore*/
 void balanceTree(struct tree *root){
     //https://www.cs.usfca.edu/~galles/visualization/AVLtree.html
     
 }
 
-struct tree *root;
 void main(void){
+    struct tree *node=NULL;
+    struct tree *root=NULL;
     char nome[sizeOfName];
 
-    root=NULL;
     do{
         printf("Digite um nome ou '0' para sair:");
         scanf("%s", nome);
-        if(!root) root=makeTree(root, root, nome);
-        else makeTree(root, root, nome);
+        node = createNewNode(node, nome);
+        insertNewNode(root, node);
     } while(strcmp(nome,"0")!=0);
-    printTree(root, 0);
+    printTree(node, 0, 1);
 }
